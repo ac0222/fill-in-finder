b3d7728aa1dd951c69dccb8e63ae1915ed02233a
adrian-c <adrian.che0222@gmail.com>
2017-07-22 22:10:34 +1000

    added stubs for sms functions

--- sms_functions.js
+++ sms_functions.js
@@ -2,6 +2,7 @@ var sprintf = require("sprintf-js").sprintf;
 
 // SMS functions
 function handle_sms(sms) {
+	// TODO replace with actual sms parsing code
 	var pnum = sms.body.phone_number;
 	var msg = sms.body.message.toLowerCase();
 	return [pnum, msg];
@@ -9,15 +10,23 @@ function handle_sms(sms) {
 
 function send_out_requests(players) {
 	for (var i = 0; i < players.length; i++) {
+		send_sms(players[i].PhoneNumber, "ay can u fill in lol");
 		console.log("An sms was sent to: " + 
 			players[i].FirstName + " " + players[i].LastName);
 	}
 }
 
 function confirm_fill_in(fir, fill_in) {
+	send_sms(fir.PhoneNumber, "we found a fill in for you, ya scrub");
+	send_sms(fill_in.PhoneNumber, "k ur filling in, be there or be square");
 	console.log(sprintf("%s %s will fill in for %s %s, in %s grade",
 		fill_in.FirstName, fill_in.LastName, 
 		fir.FirstName, fir.LastName, fir.Grade));
+}
+
+function send_sms(pnum, msg) {
+	// TODO replace with actual sms sending function here
+	console.log("Phone number: " + pnum + "||" + "Message: " + msg);
 }
 
 module.exports = {
